5cc2ad03aef9007882db4b99a0cc41aa4b239aa7
Sanket Salve <sanket.r.salve@gmail.com>
2024-09-22 00:55:10 +0530

    Added Firebase Project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lingo_news/core/theme/app_theme.dart';
 import 'package:lingo_news/features/authentication/presentation/auth_screen.dart';
+import 'package:lingo_news/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
